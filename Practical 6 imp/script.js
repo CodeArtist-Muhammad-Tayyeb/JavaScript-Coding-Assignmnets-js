8d0f917fc9ac90ae8ae921de1c6a211a551bcc86
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2024-07-05 16:29:24 +0500

    Practical 6 imp Assignment

---
+++ Practical 6 imp/script.js
@@ -0,0 +1,51 @@
+// -------------------------------REMOVE EXTRA SPACE FROM STRING----------------------------------
+// let fullName = " Hello World ";
+// fullName = fullName.trim();
+// console.log(fullName);
+
+// -------------------------------GETTING THE SUBSTRING-------------------------------------------
+// let text = "Hello World";
+// text = text.substring(5, 11);
+// console.log(text);
+
+// -------------------------------EXTRACT THE CHARACTER FROM STRING---------------------------------
+// let text = "Hello World";
+// text = text.slice(6, 11);
+// console.log(text);
+
+// -------------------------------SPLITTING THE STRING--------------------------------------------
+// let fullName = "hello-world";
+// fullName = fullName.split("-");
+// console.log(fullName);
+
+// -------------------------------JION THE ARRAY INTO A STRING--------------------------------------
+// let arr = ["Apple", "Banana", "Apple"];
+// console.log(arr);
+// arr = arr.join(" ");
+// console.log(arr);
+
+// ------------------------------FIND THE INDEX OF CHARACTER----------------------------------------
+// let text = "Hello World";
+// text = text.indexOf("o");
+// console.log(text);
+
+// ------------------------------FINDING THE LASTINDEX OF STRING-----------------------------------
+// let text = "Hello World World";
+// text = text.lastIndexOf("W");
+// console.log(text);
+
+// ------------------------------CHECKING THE STRING START FROM "Wor"-------------------------------
+// let text = "World";
+// text = text.startsWith("Wor");
+// console.log(text);
+
+// ------------------------------END WITH "World"-----------------------------------------------------
+// let text = "Hello World";
+// text = text.endsWith("World");
+// console.log(text);
+
+// ------------------------------INCLUDE THE CHARACTER "o"------------------------------------------
+let text = "Hello";
+text = text.includes("o");
+console.log(text);
+
