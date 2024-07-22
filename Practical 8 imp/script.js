6a7b22d0a048224da135b1df2b21603a83d15a68
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2024-07-23 01:51:29 +0500

    prac

---
+++ Practical 8 imp/script.js
@@ -0,0 +1,71 @@
+// ---------------------------GENRATE A RANDOM NUMBERS BETWEEN 1 AND 100------------------------
+// var ranNum = Math.random();
+// var newRanNum = (ranNum * 100) + 1;
+// console.log(newRanNum);
+
+// ---------------------------GENRATE A RANDOM NUMBERS BETWEEN 2 INPUT OF USER------------------------
+// var num1 = prompt("Enter First Number: ");
+// var num2 = prompt("Enter Second Number: ");
+// var randomNum = Math.random();
+// var newNum = (randomNum *  num2) + num1;
+// console.log(newNum);
+
+// ---------------------------WHEN COIN IS TOSSED------------------------
+// var randomNum = Math.random()
+// var newNum = Math.round(randomNum);
+// if (newNum===0){
+//     console.log("Head");
+// }else{
+//     console.log("Tail");
+// }
+
+// ---------------------------GENERATE A RANDOM PASSWORD------------------------
+// var storeChar = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM1234567890`=+)(*&^%$#@!~!@#$%^&*()_<>,.{}[]:|\?/";
+// var password = "";
+// var lengthOfChar = 99;
+
+// for (let i=0; i<8; i++){
+//     var randomNum = Math.random()
+//     var newNum = Math.round(randomNum * 99); 
+//     password = password + storeChar.charAt(newNum);
+// }
+//     console.log("Strong Password: " +password);
+
+// ---------------------------STRING TO INTERGER------------------------
+// var num1 = "123";
+// var newNum = ~~num1;
+// var newNum1 = +num1;
+// var newNum2 = Number(num1);
+// var newNum3 = parseInt(num1);
+
+// console.log(newNum);
+// console.log(newNum1);
+// console.log(newNum2);
+// console.log(newNum3);
+    
+// ---------------------------STRING TO DECIMAL------------------------
+// var num1 = "123.5";
+// var newNum1 = +num1;
+// var newNum2 = Number(num1);
+// var newNum3 = parseFloat(num1);
+
+// console.log(newNum1);
+// console.log(newNum2);
+// console.log(newNum3);
+
+// ----------------------------TAKE INPUT FROM USER AND CONVERT TO INT OR DEC--------------------------
+// var newNum = prompt("Enter a String: " );
+// var num1 = +newNum;
+// console.log("The number is integer or decimal" +num1);
+
+// ----------------------------TAKE INPUT FROM USER AND CONVERT STRING TO NUMBER AND NUMBER TO STRING--------------------------
+// var num1 = "123";
+// var newNum = Number(num1);
+// console.log("String to Number: " +newNum);
+
+// var num1 = 1234;
+// var newNum = num1.toString();
+// console.log("Number to String: " +newNum);
+
+// 
+;
