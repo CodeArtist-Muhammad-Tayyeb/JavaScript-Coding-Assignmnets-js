06016042e9da9ec3b79ec6646a8fb5e2f1e23e57
CodeArtist-Muhammad-Tayyeb <22jzbcs0223@uetpeshawar.edu.pk>
2024-07-26 01:21:02 +0500

    Done JS 7

---
+++ Practical 7 imp/script.js
@@ -0,0 +1,15 @@
+/* Write a JavaScript program that asks the user for a string and an index,
+ and then displays the character at that index in the string using the javascript method. 3 marks */
+
+// var inputForString = prompt("Enter the String: ");
+// var inputForIndex = prompt("Enter the Index: ");
+
+// if (inputForIndex >= 0 && inputForIndex < inputForString.length){
+//     console.log(inputForString);
+//     console.log("The Char at Index " + inputForIndex + " is: " + inputForString.charAt(inputForIndex));
+// }else{
+//     console.log("Error: Plz Enter index between 0 and " + (inputForString.length - 1));
+// }
+
+/* Write a JavaScript program that asks the user for a string, a substring to search for, and a replacement substring, and then displays 
+the modified string with all occurrences of the search substring replaced using the replace() method. 3 marks */
